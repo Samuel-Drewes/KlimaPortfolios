f606c8fd42ec4f2f1b936f1864926b3e0a33a1ac
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-03-28 19:22:43 +0100

    added content

---
+++ klima_app/klima_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+st.title('BMZ Klima Dashboard')
+
+# Get and process Data
+merged_df = pd.read_csv('upload_data/global_df.csv')
+
+df_long = merged_df.stack().reset_index()
+df_long.columns = ['Climate Relevance', 'Year', 'Financing']
+df_long = df_long[df_long['Climate Relevance'] != 'Total Financing']
+
+# Design Fig
+
+fig = px.bar(
+    df_long,
+    x='Year',
+    y='Financing',
+    color='Climate Relevance',  # This determines the stack segments
+    hover_data=['Climate Relevance', 'Financing'],  # Customize hover info
+    title='Global BMZ Financing by Climate Relevance'
+)
+
+fig.update_layout(
+    xaxis_title='Year',
+    yaxis_title='Total Financing',
+    barmode='stack'
+)
+
+st.plotly_chart(fig)
