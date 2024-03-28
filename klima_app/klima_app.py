6ec00356aefb71e1427094daa10d1843535e2c06
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-03-28 19:25:58 +0100

    where going wrong

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -8,6 +8,8 @@ st.title('BMZ Klima Dashboard')
 # Get and process Data
 merged_df = pd.read_csv('upload_data/global_df.csv')
 
+st.dataframe(data=merged_df)
+
 df_long = merged_df.stack().reset_index()
 df_long.columns = ['Climate Relevance', 'Year', 'Financing']
 df_long = df_long[df_long['Climate Relevance'] != 'Total Financing']
