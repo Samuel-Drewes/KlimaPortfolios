a905ed0bb655534689e74c8b1363b2f2c7ddcbff
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-04-02 09:58:47 +0200

    paging 2

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -3,17 +3,16 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
+# Overview
+
 st.title('BMZ Klima Dashboard')
 
 page = st.sidebar.selectbox('Choose your page', ['Aggregate Data', 'Country Breakdown'])
 
 # Get and process Data
+
 merged_df = pd.read_csv('upload_data/global_df.csv')
-
-
 merged_df = merged_df.set_index(merged_df.columns[0])
-
-
 df_long = merged_df.stack().reset_index()
 df_long.columns = ['Climate Relevance', 'Year', 'Financing']
 df_long = df_long[df_long['Climate Relevance'] != 'Total Financing']
@@ -35,6 +34,8 @@ fig.update_layout(
     yaxis_title='Total Financing',
     barmode='stack'
 )
+
+# Display Results
 
 if page == 'Aggregate Data':
     st.header("Aggregate Data Overview")
