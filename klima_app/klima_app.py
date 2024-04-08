064478779ee68be1c6e623355aec35286372d395
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-04-08 19:09:42 +0200

    v2.3 slider 2

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -20,41 +20,7 @@ globe_df = pd.read_csv('upload_data/globe_df')
 globe_waterfall_df = pd.read_csv('upload_data/globe_waterfall.csv')
 
 
-# Design Fig Globe Bar
 
-fig_globe_bar = px.bar(globe_df, x='Year', y='Amount', color='Type',
-            title='Global Financing Totals',
-            labels={'Amount': 'Financing Amount (Euros)', 'Year': 'Year'},
-            category_orders={'Type': ['Other Funds','Climate Finance']},
-            color_discrete_map={'Other Funds': 'orange', 'Climate Finance': 'green'})# This ensures consistent color ordering
-
-fig_globe_bar.update_layout(title_x=0.5)
-
-# Design Fig Globe Waterfall
-
-initial_value = globe_waterfall_df['Percentage'].iloc[0] - globe_waterfall_df['Change'].iloc[1] # Subtract the first actual change to get the starting point
-
-
-fig_globe_waterfall = go.Figure(go.Waterfall(
-    name = "20", orientation = "v",
-    measure = ["absolute"] + ["relative"] * (len(globe_waterfall_df) - 1), # The first measure is absolute, others are relative
-    x = globe_waterfall_df['Year'].astype(str),
-    textposition = "outside",
-    text = globe_waterfall_df['Change'].round(2).astype(str),
-    y = [initial_value] + globe_waterfall_df['Change'].tolist()[1:], # The initial value plus the changes
-    connector = {"line":{"color":"rgb(63, 63, 63)"}},
-))
-
-fig_globe_waterfall.update_layout(
-        title = {
-            'text': "Yearly Percentage Change in Climate Finance",
-            'y':0.9,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top'},
-        xaxis = {"type":"category"},
-        yaxis = {"title":"Percentage"},
-)
 
 
 
@@ -67,14 +33,54 @@ fig_globe_waterfall.update_layout(
 if page == 'Global Data':
     st.header("Global Data Overview")
 
+    # Slider
+
     from_year, to_year = st.slider(
     'Select Years',
     min_value=2013,
-    max_value=2015,
-    value=[2013, 2015])
+    max_value=2022,
+    value=[2013, 2022])
 
+    # Globe Stacked 
+    
+    globe_df = globe_df[globe_df['Year'].between(from_year,to_year)]
+
+    fig_globe_bar = px.bar(globe_df, x='Year', y='Amount', color='Type',
+                title='Global Financing Totals',
+                labels={'Amount': 'Financing Amount (Euros)', 'Year': 'Year'},
+                category_orders={'Type': ['Other Funds','Climate Finance']},
+                color_discrete_map={'Other Funds': 'orange', 'Climate Finance': 'green'})# This ensures consistent color ordering
+
+    fig_globe_bar.update_layout(title_x=0.5)
 
     st.plotly_chart(fig_globe_bar)
+
+    # Design Fig Globe Waterfall
+
+    initial_value = globe_waterfall_df['Percentage'].iloc[0] - globe_waterfall_df['Change'].iloc[1] # Subtract the first actual change to get the starting point
+
+
+    fig_globe_waterfall = go.Figure(go.Waterfall(
+        name = "20", orientation = "v",
+        measure = ["absolute"] + ["relative"] * (len(globe_waterfall_df) - 1), # The first measure is absolute, others are relative
+        x = globe_waterfall_df['Year'].astype(str),
+        textposition = "outside",
+        text = globe_waterfall_df['Change'].round(2).astype(str),
+        y = [initial_value] + globe_waterfall_df['Change'].tolist()[1:], # The initial value plus the changes
+        connector = {"line":{"color":"rgb(63, 63, 63)"}},
+    ))
+
+    fig_globe_waterfall.update_layout(
+            title = {
+                'text': "Yearly Percentage Change in Climate Finance",
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+            xaxis = {"type":"category"},
+            yaxis = {"title":"Percentage"},
+    )
+
     st.plotly_chart(fig_globe_waterfall)
 
 if page == 'Country Breakdown':
