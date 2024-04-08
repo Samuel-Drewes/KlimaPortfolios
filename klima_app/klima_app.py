1180b3df143c022654b235eca0e872267ff9a3c9
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-04-08 18:08:13 +0200

    v2

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -7,89 +7,84 @@ import plotly.express as px
 
 st.title('BMZ Klima Dashboard')
 
-page = st.sidebar.selectbox('Choose your page', ['Aggregate Data', 'Country Breakdown', 'Country Breakdown New Logic'])
+page = st.sidebar.selectbox('Choose your page', ['Global Data', 'Country Breakdown', 'Country Comparison'])
 
-# Get and process Globe Data
+# Get Globe Data
 
-merged_df = pd.read_csv('upload_data/global_df.csv')
-merged_df = merged_df.set_index(merged_df.columns[0])
-df_long = merged_df.stack().reset_index()
-df_long.columns = ['Climate Relevance', 'Year', 'Financing']
-df_long = df_long[df_long['Climate Relevance'] != 'Total Financing']
+# merged_df = pd.read_csv('upload_data/global_df.csv')
+# merged_df = merged_df.set_index(merged_df.columns[0])
+# df_long = merged_df.stack().reset_index()
+# df_long.columns = ['Climate Relevance', 'Year', 'Financing']
+# df_long = df_long[df_long['Climate Relevance'] != 'Total Financing']
+
+globe_df = pd.read_csv('upload_data/globe_df')
+
 
 
 # Design Fig Globe
 
-fig = px.bar(
-    df_long,
-    x='Year',
-    y='Financing',
-    color='Climate Relevance',  # This determines the stack segments
-    hover_data=['Climate Relevance', 'Financing'],  # Customize hover info
-    title='Global BMZ Financing by Climate Relevance'
-)
+fig = px.bar(read_df, x='Year', y='Amount', color='Type',
+            title='Global Financing Totals',
+            labels={'Amount': 'Financing Amount (Euros)', 'Year': 'Year'},
+            category_orders={'Type': ['Other Funds','Climate Finance']},
+            color_discrete_map={'Other Funds': 'orange', 'Climate Finance': 'green'})# This ensures consistent color ordering
 
-fig.update_layout(
-    xaxis_title='Year',
-    yaxis_title='Total Financing',
-    barmode='stack'
-)
+fig.update_layout(title_x=0.5)
 
-# Get and process Country Specific Data
+# # Get and process Country Specific Data
 
-df_country = pd.read_csv('upload_data/country_specific_df.csv')[['Recipient Name', 'Year', 'Value']]
-countries = df_country['Recipient Name'].unique()
+# df_country = pd.read_csv('upload_data/country_specific_df.csv')[['Recipient Name', 'Year', 'Value']]
+# countries = df_country['Recipient Name'].unique()
 
-# Get and process Country Specific Data New Methodology
+# # Get and process Country Specific Data New Methodology
 
-df_country_2 = pd.read_csv('upload_data/newmethod_country_specific_df.csv')[['Recipient Name', 'Year', 'Value']]
-countries_2 = df_country_2['Recipient Name'].unique()
+# df_country_2 = pd.read_csv('upload_data/newmethod_country_specific_df.csv')[['Recipient Name', 'Year', 'Value']]
+# countries_2 = df_country_2['Recipient Name'].unique()
 
 
 
 # Display Results
 
-if page == 'Aggregate Data':
-    st.header("Aggregate Data Overview")
-    st.dataframe(data=merged_df)
+if page == 'Global Data':
+    st.header("Global Data Overview")
     st.plotly_chart(fig)
 
-if page == 'Country Breakdown':
+# if page == 'Country Breakdown':
 
-    st.header('Country Breakdown')
+#     st.header('Country Breakdown')
 
-    selected_countries = st.multiselect('Which countries would you like to view?',
-                                         countries,
-                                           ['India', 'Brazil', 'Ukraine', 'Namibia', 'South Africa'])
+#     selected_countries = st.multiselect('Which countries would you like to view?',
+#                                          countries,
+#                                            ['India', 'Brazil', 'Ukraine', 'Namibia', 'South Africa'])
     
-    selected_df = df_country[df_country['Recipient Name'].isin(selected_countries)]
+#     selected_df = df_country[df_country['Recipient Name'].isin(selected_countries)]
 
-    fig = px.line(selected_df, 
-              x='Year', 
-              y='Value', 
-              color='Recipient Name',
-              title='Value by Year for Selected Country',
-              labels={'Value': 'Climate Relevant Financing (%)', 'Year': 'Year'},
-              markers=True)
+#     fig = px.line(selected_df, 
+#               x='Year', 
+#               y='Value', 
+#               color='Recipient Name',
+#               title='Value by Year for Selected Country',
+#               labels={'Value': 'Climate Relevant Financing (%)', 'Year': 'Year'},
+#               markers=True)
     
-    st.plotly_chart(fig)
+#     st.plotly_chart(fig)
 
-if page == 'Country Breakdown New Logic':
+# if page == 'Country Breakdown New Logic':
 
-    st.header('Country Breakdown New Logic')
+#     st.header('Country Breakdown New Logic')
 
-    selected_countries = st.multiselect('Which countries would you like to view?',
-                                         countries_2,
-                                           ['India', 'Brazil', 'Ukraine', 'Namibia', 'South Africa'])
+#     selected_countries = st.multiselect('Which countries would you like to view?',
+#                                          countries_2,
+#                                            ['India', 'Brazil', 'Ukraine', 'Namibia', 'South Africa'])
     
-    selected_df = df_country_2[df_country_2['Recipient Name'].isin(selected_countries)]
+#     selected_df = df_country_2[df_country_2['Recipient Name'].isin(selected_countries)]
 
-    fig = px.line(selected_df, 
-              x='Year', 
-              y='Value', 
-              color='Recipient Name',
-              title='Value by Year for Selected Country',
-              labels={'Value': 'Climate Relevant Financing (%)', 'Year': 'Year'},
-              markers=True)
+#     fig = px.line(selected_df, 
+#               x='Year', 
+#               y='Value', 
+#               color='Recipient Name',
+#               title='Value by Year for Selected Country',
+#               labels={'Value': 'Climate Relevant Financing (%)', 'Year': 'Year'},
+#               markers=True)
     
-    st.plotly_chart(fig)
+#     st.plotly_chart(fig)
