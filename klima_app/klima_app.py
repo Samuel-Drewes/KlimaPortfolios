5411bb6efa866684567eeee3c7ad8022852d3b53
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-04-29 15:50:45 +0200

    combined changes v12.3 fix 8

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -41,6 +41,7 @@ def stacked_area_chart(full_sector_df, year_start, year_end, category, top_n_sec
     filtered_col_df['Grouped Sector'] = filtered_col_df['Sector'].apply(lambda x: x if x in top_sectors else 'Andere Sektoren')
     grouped_df = filtered_col_df.groupby('Grouped Sector').sum().reset_index()
 
+    return grouped_df
     
     # Prepare for Plotting
     
@@ -314,9 +315,9 @@ if page == 'Gesamtübersicht':
     if st.button("Sektorübersicht erstellen"):
 
         show_fig = stacked_area_chart(sector_df, from_year, to_year, category, top_n_sectors, abs_or_perc)
-        # st.dataframe(show_fig)
+        st.dataframe(show_fig)
 
-        st.plotly_chart(show_fig)
+        # st.plotly_chart(show_fig)
         st.write(f"Flächendiagramm generiert von {from_year} bis {to_year} für {category}, {top_n_sectors} Top-Sektoren, Anzeigeart: {abs_or_perc}.")
 
 
