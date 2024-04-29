8478e89363565550836e8372cd5253261987e020
Samuel Drewes <samueldrewes@Samuels-MacBook-Air-3.local>
2024-04-29 15:55:20 +0200

    combined changes v12.3 fix 11

--- klima_app/klima_app.py
+++ klima_app/klima_app.py
@@ -47,7 +47,7 @@ def stacked_area_chart(full_sector_df, year_start, year_end, category, top_n_sec
     long_df['Year'] = long_df['Year'].str.extract('(\d+)')
     long_df['Year'] = pd.to_numeric(long_df['Year'])
 
-    return long_df.dropna()
+    long_df =  long_df.dropna()
     
     # Year Select
     
@@ -315,9 +315,9 @@ if page == 'Gesamtübersicht':
     if st.button("Sektorübersicht erstellen"):
 
         show_fig = stacked_area_chart(sector_df, from_year, to_year, category, top_n_sectors, abs_or_perc)
-        st.dataframe(show_fig)
+        # st.dataframe(show_fig)
 
-        # st.plotly_chart(show_fig)
+        st.plotly_chart(show_fig)
         st.write(f"Flächendiagramm generiert von {from_year} bis {to_year} für {category}, {top_n_sectors} Top-Sektoren, Anzeigeart: {abs_or_perc}.")
 
 
